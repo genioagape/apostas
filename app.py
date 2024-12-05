90527b99a66721fafd83e74ae9a5b42876f9f46c
genioagape <genioagape@gmail.com>
2024-12-05 16:11:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -42,5 +42,6 @@ def search_google(query):
         return f"Erro na pesquisa: {str(e)}"
 
 if __name__ == '__main__':
-    port = os.getenv('PORT', 10000)
+    port = os.getenv('PORT', 8080)
     app.run(host='0.0.0.0', port=port)
+
